fab1fe40211beffb80e004bea07a0fe33e634ebe
whyimsobroke <aldisgailis1@gmail.com>
2022-12-22 07:21:19 +0200

    styled header to look better

--- pic-gen/src/components/header_components/HeaderWrapper.svelte
+++ pic-gen/src/components/header_components/HeaderWrapper.svelte
@@ -1,4 +1,6 @@
-<div>
-  <h1>IMG'ne Gen</h1>
-  <p>Generate images from text using OpenAi API!</p>
+<div class="text-center">
+  <h1 class="text-4xl font-bold text-gray-900">IMG'ne Gen</h1>
+  <p class="text-lg text-gray-500">
+    Generate images from text using <b class="text-purple-600">OpenAi API</b>!
+  </p>
 </div>
--- pic-gen/src/components/search_components/SearchButton.svelte
+++ pic-gen/src/components/search_components/SearchButton.svelte
@@ -1,5 +1,6 @@
 <div>
-  <button class="px-5 py-3 bg-indigo-500 color-white rounded-md text-white"
+  <button
+    class="px-5 py-3 bg-indigo-500 color-white rounded-md text-white font-bold"
     >Search</button
   >
 </div>
